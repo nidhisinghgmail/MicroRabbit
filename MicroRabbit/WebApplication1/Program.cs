454fbbc46db2e4087f6e3127583be48de33015f3
nidhi.singh3 <nidhi.singh3@globallogic.com>
2020-09-14 16:35:14 +0530

    Updated versions and wrote classes again

--- MicroRabbit/WebApplication1/Program.cs
+++ MicroRabbit/WebApplication1/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace MicroRabbit.Banking.API
+namespace WebApplication1
 {
     public class Program
     {
--- MicroRabbit/WebApplication1/Startup.cs
+++ MicroRabbit/WebApplication1/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace MicroRabbit.Banking.API
+namespace WebApplication1
 {
     public class Startup
     {
@@ -25,7 +25,7 @@ namespace MicroRabbit.Banking.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +37,7 @@ namespace MicroRabbit.Banking.API
             }
             else
             {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
